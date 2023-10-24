f01662d5eeb7f3a043fb6c4a83e576266dde2393
Shubh1am <88609379+Shubh1am@users.noreply.github.com>
2023-10-24 11:58:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,3 @@ resource "aws_instance" "my-machine" {
   }
 }
 
-
-or 
-
-resource "aws_instance" "example" {
-    ami  = "ami-03eb6185d756497f8"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
-    subnet_id = "subnet-02f912683905859bf"
-    key_name = "sm"
-}
