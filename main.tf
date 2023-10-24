07a65e821348c48d87a08a40c96d88b966f06789
Shubh1am <88609379+Shubh1am@users.noreply.github.com>
2023-10-24 13:00:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-resource "aws_security_group" "sm" {
-  name   = "sm"
-}
-
-
 resource "aws_instance" "my-machine" {
 
   count = 2
@@ -12,13 +7,12 @@ resource "aws_instance" "my-machine" {
   instance_type = var.instance_type
   subnet_id = "subnet-02f912683905859bf"
   key_name = "sm"
-
+  
+  
   tags = {
    Name = "ec2-${count.index}"
   }
-security_groups = [
-    aws_security_group.existing_security_group.name
-]
 }
+
 
 
